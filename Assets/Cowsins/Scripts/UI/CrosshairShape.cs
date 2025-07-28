ccc51b953fd42458c4b354fc5f531add0202f182
IgorDavidovich <sir.funtik18@gmail.com>
2025-07-28 22:02:23 +0700

    Added FPS Engine

---
+++ Assets/Cowsins/Scripts/UI/CrosshairShape.cs
@@ -0,0 +1,29 @@
+#if UNITY_EDITOR
+using UnityEditor.Presets;
+#endif
+using UnityEngine;
+
+namespace cowsins
+{
+    [System.Serializable]
+    public class CrosshairParts
+    {
+        public bool topPart = true, downPart = true, leftPart = true, rightPart = true, center = false;
+    }
+
+    public class CrosshairShape : MonoBehaviour
+    {
+        [SerializeField] private CrosshairParts defaultCrosshairParts;
+
+        private CrosshairParts currentCrosshairParts;
+
+        public CrosshairParts DefaultCrosshairParts => defaultCrosshairParts;
+        public CrosshairParts CurrentCrosshairParts => currentCrosshairParts;
+
+        public string presetName;
+
+        private void Awake() => SetCrosshair(defaultCrosshairParts);
+        public void SetCrosshair(CrosshairParts parts) => currentCrosshairParts = parts;
+        public void ResetCrosshairToDefault() => currentCrosshairParts = defaultCrosshairParts;
+    }
+}
